b19e184d4a2d1f9fdb8497406ef6df7d930ee5b5
ivagliano <i.vagliano@zbw.eu>
2018-07-03 11:11:26 +0200

    Fixes in plotting citation distribution for and citation distribution for acm

--- stats.py
+++ stats.py
@@ -19,6 +19,20 @@ def plot(objects, dataset, title):
     plt.savefig('papers_by{}_{}.pdf'.format(title, dataset))
 
 
+def paper_by_n_citations(citations):
+    '''
+    From a dictionary with paper IDs as keys and citation numbers as values
+    to a dictionary with citation numbers as keys and paper numbers as values
+    '''
+    papers_by_citations = {}
+    for paper in citations.keys()
+        try:
+            papers_by_citations[citations[paper]] += 1
+        except KeyError:
+            papers_by_citations[citations[paper]] += 1
+
+    return papers_by_citations
+
 # path = '../Data/Economics/econbiz62k.tsv'
 path = '/data21/lgalke/PMC/citations_pmc.tsv'
 dataset = "dblp"
@@ -38,11 +52,18 @@ if dataset == "dblp" or dataset == "acm":
             try:
                 citations[paper["n_citation"]] += 1
             except KeyError:
-                citations = [paper["n_citation"]] = 0
+                citations[paper["n_citation"]] = 1
+        else:
+            for ref in paper["references"]:
+                try:
+                    citations[ref] += 1
+                except KeyError:
+                    citations[ref] = 1
 
     plot(years, dataset, "year")
-    if dataset == "dblp":
-        plot(citations, dataset, "number of citations")
+    if dataset == "acm":
+        citations = paper_by_n_citations(citations)
+    plot(citations, dataset, "number of citations")
 
     print("Unpacking {} data...".format(dataset))
     bags_of_papers, ids, side_info = unpack_papers(papers)
