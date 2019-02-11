Fixed a bug in normalization

aaerec/vae.py
@@ -51,8 +51,6 @@ class VAE(nn.Module):
         super(VAE, self).__init__()
 
         self.normalize_inputs = normalize_inputs
-        if self.normalize_inputs:
-            inp = F.normalize(inp, 1)
         self.inp = inp
         self.n_hidden = n_hidden
         self.n_code = n_code
@@ -95,7 +93,9 @@ class VAE(nn.Module):
         return self.final_act(self.fc4(h3))
 
     def forward(self, x):
-        # TODO could I use x instead of self.inp?
+        if self.normalize_inputs:
+            inp = F.normalize(x, 1)
+        # TODO could I use x instead of self.inp? Do we need x.view?
         mu, logvar = self.encode(x.view(-1, self.inp))
         z = self.reparametrize(mu, logvar)
         return self.decode(z), mu, logvar