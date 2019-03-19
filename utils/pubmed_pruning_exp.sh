c3ec58654b39b5ac80e03d8ec80a1801436e7461
mapo89 <iacopo.va@gmail.com>
2019-03-19 17:46:42 +0100

    Updated version of running script for pubmed/econbiz

--- utils/pubmed_pruning_exp.sh
+++ utils/pubmed_pruning_exp.sh
@@ -1,9 +1,11 @@
-DATASET_PATH=../Data/PMC/citations_pmc.tsv
+DATASET_PATH=/data21/lgalke/datasets/citations_pmc.tsv
 DATASET_YEAR=2011
-OUTPUT_PREFIX=final_results/run3/pubmed
+OUTPUT_PREFIX=/data22/ivagliano/cit2vec-journal-results/pubmed-vae-dae/titles-only
+RUN=1
 mkdir -p $OUTPUT_PREFIX
-for THRESHOLD in 55 50 45 40 35 30 25 20 15
+for THRESHOLD in 50 45 40 35 30 25 20 15 10
 do
-  python3 main.py $DATASET_PATH $DATASET_YEAR -m $THRESHOLD -o $OUTPUT_PREFIX/m$THRESHOLD.log
+  echo python3 ../main.py $DATASET_PATH $DATASET_YEAR -m $THRESHOLD -o $OUTPUT_PREFIX/pubmed-$DATASET_YEAR-$THRESHOLD-$RUN.txt
+  python3 ../main.py $DATASET_PATH $DATASET_YEAR -m $THRESHOLD -o $OUTPUT_PREFIX/pubmed-$DATASET_YEAR-$THRESHOLD-$RUN.txt
 done
 exit 0
