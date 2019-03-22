ef229d870d5b1a5b2b54dd832ed21f545ebdfe39
ivagliano <i.vagliano@zbw.eu>
2019-03-22 15:29:36 +0100

    Adaptation of irgan to Recommender interface

--- irgan/cf_gan.py
+++ irgan/cf_gan.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
-from dis_model import DIS
-from gen_model import GEN
+from irgan.dis_model import DIS
+from irgan.gen_model import GEN
 import cPickle
 import numpy as np
-import utils as ut
+import irgan.utils as ut
 import multiprocessing
+import argparse
 
 # own recommender stuff
 from aaerec.base import Recommender
@@ -28,77 +29,39 @@ all_items = set(range(ITEM_NUM))
 workdir = 'ml-100k/'
 DIS_TRAIN_FILE = workdir + 'dis-train.txt'
 
-#########################################################################################
-# Load data
-#########################################################################################
-user_pos_train = {}
-with open(workdir + 'movielens-100k-train.txt')as fin:
-    for line in fin:
-        line = line.split()
-        uid = int(line[0])
-        iid = int(line[1])
-        r = float(line[2])
-        if r > 3.99:
-            if uid in user_pos_train:
-                user_pos_train[uid].append(iid)
-            else:
-                user_pos_train[uid] = [iid]
-
-user_pos_test = {}
-with open(workdir + 'movielens-100k-test.txt')as fin:
-    for line in fin:
-        line = line.split()
-        uid = int(line[0])
-        iid = int(line[1])
-        r = float(line[2])
-        if r > 3.99:
-            if uid in user_pos_test:
-                user_pos_test[uid].append(iid)
-            else:
-                user_pos_test[uid] = [iid]
-
-all_users = user_pos_train.keys()
-all_users.sort()
-
-
 class IRGAN():
 
     def __init__(self,
-                 inp,
-                 out,
-                 n_hidden=100,
-                 n_code=50,
+                 # inp,
+                 gen_param,
+                 batch_size=16,
+                 emb_dim=5,
                  lr=0.001,
-                 batch_size=100,
-                 n_epochs=500,
-                 optimizer='adam',
+                 init_delta=0.05,
+                 g_epochs=50,
+                 d_epochs=100,
+                 epochs=15,
                  normalize_inputs=True,
-                 activation='ReLU',
-                 final_activation='Sigmoid',
-                 # TODO try later
-                 # dropout=(.2,.2),
                  conditions=None,
-                 verbose=True,
-                 log_interval=1):
+                 verbose=True):
 
-
-        self.normalize_inputs = normalize_inputs
-        self.inp = inp
-        self.n_hidden = n_hidden
-        self.n_code = n_code
-        self.n_epochs = n_epochs
+        # self.normalize_inputs = normalize_inputs
+        # self.inp = inp
         self.verbose = verbose
-        # TODO try later
-        # self.dropout = dropout
         self.batch_size = batch_size
-        self.lr = lr
-        self.activation = activation
         self.conditions = conditions
+        self.emb_dim = emb_dim
+        self.lr = lr
+        self.init_delta = init_delta
+        self.gen_param = gen_param
+        self.epochs = epochs
+        self.g_epochs = g_epochs
+        self.d_epochs = d_epochs
 
-        self.generator = GEN(ITEM_NUM, USER_NUM, EMB_DIM, lamda=0.0 / BATCH_SIZE, param=param, initdelta=INIT_DELTA,
-                             learning_rate=0.001)
-        self.discriminator = DIS(ITEM_NUM, USER_NUM, EMB_DIM, lamda=0.1 / BATCH_SIZE, param=None, initdelta=INIT_DELTA,
-                                 learning_rate=0.001)
+        self.generator = GEN(ITEM_NUM, USER_NUM, emb_dim, lamda=0.0 / batch_size, param=gen_param, initdelta=init_delta,
+                             learning_rate=lr)
+        self.discriminator = DIS(ITEM_NUM, USER_NUM, emb_dim, lamda=0.1 / batch_size, param=None, initdelta=init_delta,
+                                 learning_rate=lr)
 
     def dcg_at_k(self, r, k):
         r = np.asfarray(r)[:k]
@@ -175,19 +138,17 @@ class IRGAN():
         sess = tf.Session(config=config)
         sess.run(tf.global_variables_initializer())
 
-        print
-        "gen ", self.predict(sess, self.generator)
-        print
-        "dis ", self.predict(sess, self.discriminator)
+        print "gen ", self.predict(sess, self.generator)
+        print "dis ", self.predict(sess, self.discriminator)
 
         dis_log = open(workdir + 'dis_log.txt', 'w')
         gen_log = open(workdir + 'gen_log.txt', 'w')
 
         # minimax training
         best = 0.
-        for epoch in range(15):
+        for epoch in range(self.epochs):
             if epoch >= 0:
-                for d_epoch in range(100):
+                for d_epoch in range(self.d_epochs):
                     if d_epoch % 5 == 0:
                         self.generate_for_d(sess, self.generator, DIS_TRAIN_FILE)
                         train_size = ut.file_len(DIS_TRAIN_FILE)
@@ -207,7 +168,7 @@ class IRGAN():
                                                 self.discriminator.label: input_label})
 
                 # Train G
-                for g_epoch in range(50):  # 50
+                for g_epoch in range(self.g_epochs):  # 50
                     for u in user_pos_train:
                         sample_lambda = 0.2
                         pos = user_pos_train[u]
@@ -233,16 +194,14 @@ class IRGAN():
                                      {self.generator.u: u, self.generator.i: sample, self.generator.reward: reward})
 
                     result = self.predict(sess, self.generator)
-                    print
-                    "epoch ", epoch, "gen: ", result
+                    print "epoch ", epoch, "gen: ", result
                     buf = '\t'.join([str(x) for x in result])
                     gen_log.write(str(epoch) + '\t' + buf + '\n')
                     gen_log.flush()
 
                     p_5 = result[1]
                     if p_5 > best:
-                        print
-                        'best: ', result
+                        print 'best: ', result
                         best = p_5
                         self.generator.save_model(sess, "ml-100k/gan_generator.pkl")
 
@@ -296,7 +255,7 @@ class IRGANRecommender(Recommender):
     verbose: Print losses during training
     normalize_inputs: Whether l1-normalization is performed on the input
     """
-    def __init__(self, conditions=None, **kwargs):
+    def __init__(self, gen_param, conditions=None, **kwargs):
         """ tfidf_params get piped to either TfidfVectorizer or
         EmbeddedVectorizer.  Remaining kwargs get passed to
         AdversarialAutoencoder """
@@ -304,6 +263,7 @@ class IRGANRecommender(Recommender):
         self.verbose = kwargs.get('verbose', True)
         self.conditions = conditions
         self.model_params = kwargs
+        self.gen_param = gen_param
 
     def __str__(self):
        desc = "IRGAN"
@@ -330,13 +290,12 @@ class IRGANRecommender(Recommender):
         else:
             condition_data = None
 
-        self.model = IRGAN(conditions=self.conditions, **self.model_params)
+        self.model = IRGAN(self.gen_param, conditions=self.conditions, **self.model_params)
 
         print(self)
         print(self.model)
         print(self.conditions)
 
-        # gives (Adversarial) Autoencoder BaseData (--> X: <???> representation) and side_info (attr_vect: numpy)
         self.model.fit(X, condition_data=condition_data)
 
     def predict(self, test_set):
@@ -355,87 +314,63 @@ class IRGANRecommender(Recommender):
 
 
 def main():
+    #########################################################################################
+    # Load data
+    #########################################################################################
+    user_pos_train = {}
+    with open(workdir + 'movielens-100k-train.txt')as fin:
+        for line in fin:
+            line = line.split()
+            uid = int(line[0])
+            iid = int(line[1])
+            r = float(line[2])
+            if r > 3.99:
+                if uid in user_pos_train:
+                    user_pos_train[uid].append(iid)
+                else:
+                    user_pos_train[uid] = [iid]
+
+    user_pos_test = {}
+    with open(workdir + 'movielens-100k-test.txt')as fin:
+        for line in fin:
+            line = line.split()
+            uid = int(line[0])
+            iid = int(line[1])
+            r = float(line[2])
+            if r > 3.99:
+                if uid in user_pos_test:
+                    user_pos_test[uid].append(iid)
+                else:
+                    user_pos_test[uid] = [iid]
+
+    all_users = user_pos_train.keys()
+    all_users.sort()
+
     print "load model..."
     param = cPickle.load(open(workdir + "model_dns_ori.pkl"))
-    generator = GEN(ITEM_NUM, USER_NUM, EMB_DIM, lamda=0.0 / BATCH_SIZE, param=param, initdelta=INIT_DELTA,
-                    learning_rate=0.001)
-    discriminator = DIS(ITEM_NUM, USER_NUM, EMB_DIM, lamda=0.1 / BATCH_SIZE, param=None, initdelta=INIT_DELTA,
-                        learning_rate=0.001)
 
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    sess = tf.Session(config=config)
-    sess.run(tf.global_variables_initializer())
+    CONFIG = {
+        'pub': ('../Data/PMC/citations_pmc.tsv', 2011, 50),
+        'eco': ('../Data/Economics/econbiz62k.tsv', 2012, 1)
+    }
 
-    print "gen ", simple_test(sess, generator)
-    print "dis ", simple_test(sess, discriminator)
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument('data', type=str, choices=['pub', 'eco'])
+    args = PARSER.parse_args()
+    DATA = CONFIG[args.data]
+    logfile = 'results/' + args.data + '-decoder.log'
+    bags = Bags.load_tabcomma_format(DATA[0])
+    c_year = DATA[1]
 
-    dis_log = open(workdir + 'dis_log.txt', 'w')
-    gen_log = open(workdir + 'gen_log.txt', 'w')
+    evaluate = Evaluation(bags,
+                          year=c_year,
+                          logfile=logfile).setup(min_count=DATA[2],
+                                                 min_elements=2)
+    # print("Loading pre-trained embedding", W2V_PATH)
+    # vectors = KeyedVectors.load_word2vec_format(W2V_PATH, binary=W2V_IS_BINARY)
 
-    # minimax training
-    best = 0.
-    for epoch in range(15):
-        if epoch >= 0:
-            for d_epoch in range(100):
-                if d_epoch % 5 == 0:
-                    generate_for_d(sess, generator, DIS_TRAIN_FILE)
-                    train_size = ut.file_len(DIS_TRAIN_FILE)
-                index = 1
-                while True:
-                    if index > train_size:
-                        break
-                    if index + BATCH_SIZE <= train_size + 1:
-                        input_user, input_item, input_label = ut.get_batch_data(DIS_TRAIN_FILE, index, BATCH_SIZE)
-                    else:
-                        input_user, input_item, input_label = ut.get_batch_data(DIS_TRAIN_FILE, index,
-                                                                                train_size - index + 1)
-                    index += BATCH_SIZE
-
-                    _ = sess.run(discriminator.d_updates,
-                                 feed_dict={discriminator.u: input_user, discriminator.i: input_item,
-                                            discriminator.label: input_label})
-
-            # Train G
-            for g_epoch in range(50):  # 50
-                for u in user_pos_train:
-                    sample_lambda = 0.2
-                    pos = user_pos_train[u]
-
-                    rating = sess.run(generator.all_logits, {generator.u: u})
-                    exp_rating = np.exp(rating)
-                    prob = exp_rating / np.sum(exp_rating)  # prob is generator distribution p_\theta
-
-                    pn = (1 - sample_lambda) * prob
-                    pn[pos] += sample_lambda * 1.0 / len(pos)
-                    # Now, pn is the Pn in importance sampling, prob is generator distribution p_\theta
-
-                    sample = np.random.choice(np.arange(ITEM_NUM), 2 * len(pos), p=pn)
-                    ###########################################################################
-                    # Get reward and adapt it with importance sampling
-                    ###########################################################################
-                    reward = sess.run(discriminator.reward, {discriminator.u: u, discriminator.i: sample})
-                    reward = reward * prob[sample] / pn[sample]
-                    ###########################################################################
-                    # Update G
-                    ###########################################################################
-                    _ = sess.run(generator.gan_updates,
-                                 {generator.u: u, generator.i: sample, generator.reward: reward})
-
-                result = simple_test(sess, generator)
-                print "epoch ", epoch, "gen: ", result
-                buf = '\t'.join([str(x) for x in result])
-                gen_log.write(str(epoch) + '\t' + buf + '\n')
-                gen_log.flush()
-
-                p_5 = result[1]
-                if p_5 > best:
-                    print 'best: ', result
-                    best = p_5
-                    generator.save_model(sess, "ml-100k/gan_generator.pkl")
-
-    gen_log.close()
-    dis_log.close()
+    models = [IRGANRecommender(param)]
+    evaluate(models)
 
 
 if __name__ == '__main__':
