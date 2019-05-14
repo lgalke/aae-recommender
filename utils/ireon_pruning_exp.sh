51e4323a3a0fdf0f91d51d54acd3df14903bd64b
ivagliano <i.vagliano@zbw.eu>
2019-05-14 12:43:18 +0200

    Proper thresholds in reuters and ireon shell scripts

--- utils/ireon_pruning_exp.sh
+++ utils/ireon_pruning_exp.sh
@@ -1,9 +1,9 @@
 DATASET_YEAR=2016
-OUTPUT_PREFIX=/data22/ivagliano/cit2vec-journal-results
+OUTPUT_PREFIX=/data22/ivagliano/cit2vec-journal-results/ireon-vae-dae/titles-only
 RUN=3
 mkdir -p $OUTPUT_PREFIX
 # epochs 20
-for THRESHOLD in 50 45 40 35 30 25 20 15 10
+for THRESHOLD in 20 15 10 5 4 3 2 1
 do
   echo python3 ../eval/fiv.py $DATASET_YEAR -m $THRESHOLD -o $OUTPUT_PREFIX/ireon-$DATASET_YEAR-$THRESHOLD-$RUN.txt
   python3 ../eval/fiv.py $DATASET_YEAR -m $THRESHOLD -o $OUTPUT_PREFIX/ireon-$DATASET_YEAR-$THRESHOLD-$RUN.txt
