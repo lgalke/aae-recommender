Fix errors on using average item embeddings instead of user embeddings in IRGAN

irgan/cf_gan.py
@@ -205,9 +205,9 @@ class IRGAN():
                             reward = torch.tensor(reward)
                         if use_condition:
                             c_batch = [c[u] for c in condition_data]
-                            G_loss = self.generator(u, sample, reward, c_batch)
+                            G_loss = self.generator(self.user_pos_train[u], sample, reward, c_batch)
                         else:
-                            G_loss = self.generator(u, sample, reward)
+                            G_loss = self.generator(self.user_pos_train[u], sample, reward)
                         self.generator.step(G_loss)
 
                     if self.verbose:
@@ -229,9 +229,11 @@ class IRGAN():
             user_batch = test_users[index:index + batch_size]
             if use_condition:
                 c_batch = [c[index:index + batch_size] for c in condition_data]
-            index += batch_size
+                user_batch_rating = self.generator.all_rating([X[u] for u in user_batch], c_batch, impose_dim=1)
+            else:
+                user_batch_rating = self.generator.all_rating([X[u] for u in user_batch], impose_dim=1)
 
-            user_batch_rating = self.generator.all_rating(X[user_batch], c_batch, impose_dim=1)
+            index += batch_size
 
             user_batch_rating = user_batch_rating.detach_().cpu().numpy()
             for user_batch_rating_uid in zip(user_batch_rating, user_batch):
@@ -346,6 +348,7 @@ def main():
     user_num = evaluate.train_set.size()[0] + evaluate.test_set.size()[0]
     item_num = evaluate.train_set.size()[1]
     models = [IRGANRecommender(user_num, item_num, g_epochs=1, d_epochs=1, n_epochs=1, conditions=CONDITIONS)]
+    # models = [IRGANRecommender(user_num, item_num, g_epochs=1, d_epochs=1, n_epochs=1, conditions=None)]
     evaluate(models)
 
 

irgan/dis_model.py
@@ -50,10 +50,10 @@ class Discriminator(nn.Module):
         u_embedding = torch.zeros([len(user_pos), self.emb_dim], dtype=torch.float32)
         if torch.cuda.is_available():
             u_embedding = u_embedding.cuda()
-        for u in user_pos:
+        for idx,u in enumerate(user_pos):
             for i in u:
-                u_embedding[u].add(self.D_item_embeddings[i])
-            u_embedding[u] /= len(u)
+                u_embedding[idx].add(self.D_item_embeddings[i])
+            u_embedding[idx] /= len(u)
         if self.conditions:
             # In generator need to use dimension 0 in discriminator 1 so by default 0 (given in condition creation)
             # and here we use one through the dim parameter

irgan/gen_model.py
@@ -56,10 +56,10 @@ class Generator(nn.Module):
             u_embedding = torch.zeros([len(user_pos), self.emb_dim], dtype=torch.float32)
             if torch.cuda.is_available():
                 u_embedding = u_embedding.cuda()
-            for u in user_pos:
+            for idx,u in enumerate(user_pos):
                 for i in u:
-                    u_embedding[u].add(self.G_item_embeddings[i])
-                u_embedding[u] /= len(u)
+                    u_embedding[idx].add(self.G_item_embeddings[i])
+                u_embedding[idx] /= len(u)
         item_embeddings = self.G_item_embeddings
 
         if self.conditions: