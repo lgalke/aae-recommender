0491730fb88a2e54dc44e9a4f97a04e766898ac4
ivagliano <i.vagliano@zbw.eu>
2019-07-22 14:21:34 +0200

    Plotting distribution of papers' citations/labels' occurrence for all the datasets

--- utils/stats.py
+++ utils/stats.py
@@ -8,20 +8,34 @@ import matplotlib.pyplot as plt
 from aaerec.datasets import Bags
 from eval.aminer import unpack_papers, papers_from_files
 from eval.fiv import load, unpack_papers as unpack_papers_fiv
-
+from eval.mpd.mpd import playlists_from_slices, unpack_playlists
+import pandas as pd
 
 def compute_stats(A):
     return A.shape[1], A.min(), A.max(), np.median(A, axis=1)[0,0], A.mean(), A.std()
 
 
-def plot(objects, dataset, title, min_key):
-    if min_key != -1:
-        objects = {x : objects[x] for x in objects if x >= min_key}
-    y_pos = np.arange(len(objects.keys()))
-    plt.bar(y_pos, objects.values(), align='center', alpha=0.5)
-    plt.xticks(y_pos, objects.keys(), rotation='vertical')
-    plt.ylabel('Papers')
-    plt.title('Papers by {}'.format(title))
+def plot(objects, dataset, title, x=None):
+    # y_pos = np.arange(len(objects.keys()))
+    plt.bar(objects.keys(), objects.values(), align='center', alpha=0.5)
+    # plt.xticks(y_pos, objects.keys(), rotation='vertical')
+    if dataset == "mpd":
+        label = "Tracks"
+    elif dataset =="swp" or "rcv" or "econis":
+        label = 'Labels'
+    else:
+        label = 'Papers'
+    plt.ylabel(label)
+    # plt.title('Papers by {}'.format(title))
+    plt.xlabel(title)
+
+    # print the y value of bar at a given x
+    if x != None:
+        for x_i, y_i in enumerate(objects.values()):
+            if x_i == x:
+                print("For x={} y={}".format(x, y_i))
+                plt.text(x_i, y_i, str(y_i) + "\n", ha='center')
+
     plt.savefig('papers_by_{}_{}.pdf'.format(title.replace(" ", "_"), dataset))
     # plt.show()
     plt.close()
@@ -41,29 +55,60 @@ def paper_by_n_citations(citations):
 
     return papers_by_citations
 
+
+def from_to_key(objects, min_key, max_key=None):
+    '''
+    From a dictionary create a new dictionary with only items with key greater
+    than min_key and optionally smaller than max_key. If min_key = max_key,
+    it returns a dictionaly with only that key
+    '''
+    print("Filtering dictionary's keys from {} to {}".format(min_key, max_key))
+    # It assumes only positive integers as keys
+    if max_key != None and max_key < min_key:
+        print("Error: max_key has to be greater than min_key. Dictionary unchanged")
+        return objects
+
+    if  max_key == None and min_key <= 0:
+        print("Warning: min_key lower or equal to 0 and no max_key has no effect."
+            + "Dictionary unchanged")
+        return objects
+
+    if max_key != None:
+        return {x : objects[x] for x in objects if x >= min_key and x <= max_key}
+
+    return {x : objects[x] for x in objects if x >= min_key}
+
+
 # path = '/data21/lgalke/datasets/econbiz62k.tsv'
 # path = '/data21/lgalke/datasets/PMC/citations_pmc.tsv'
 path = '/data22/ivagliano/Reuters/rcv1.tsv'
 dataset = "rcv"
 
-if dataset == "dblp" or dataset == "acm" or dataset == "swp":
-    if dataset != "swp":
+if dataset == "dblp" or dataset == "acm" or dataset == "swp" or  dataset == "mpd":
+    if dataset != "swp" and dataset != "mpd":
         path = '/data22/ivagliano/aminer/'
         path += ("dblp-ref/" if dataset == "dblp" else "acm.txt")
+        print("Loading {} dataset".format(dataset.upper()))
         papers = papers_from_files(path, dataset, n_jobs=1)
-    else:
+    elif dataset == "swp":
         print("Loading SWP dataset")
         papers = load("/data22/ivagliano/SWP/FivMetadata_clean.json")
+    else:
+        print("Loading MPD dataset")
+        # actually not papers but playlists
+        papers = playlists_from_slices("/data21/lgalke/datasets/MPD/data/", n_jobs=4)
 
     years, citations = {}, {}
     for paper in papers:
         try:
              years[paper["year"]] += 1 
         except KeyError:
-            if "year" not in paper.keys():
+            if "year" not in paper.keys() and dataset != "mpd":
                 # skip papers without a year
+                # unless dataset is MPD, which has no year
                 continue
-            years[paper["year"]] = 0
+            if dataset != "mpd":
+                years[paper["year"]] = 0
         if dataset == "dblp":
             try:
                 citations[paper["n_citation"]] += 1
@@ -77,7 +122,7 @@ if dataset == "dblp" or dataset == "acm" or dataset == "swp":
                     citations[ref] += 1
                 except KeyError:
                     citations[ref] = 1
-        else:
+        elif dataset == "swp":
             if "subjects" not in paper.keys():
                 continue
             for subject in paper["subjects"]:
@@ -85,38 +130,90 @@ if dataset == "dblp" or dataset == "acm" or dataset == "swp":
                     citations[subject] += 1
                 except KeyError:
                     citations[subject] = 1
+        else:
+            for track in paper["tracks"]:
+                try:
+                    citations[track["track_uri"]] += 1
+                except KeyError:
+                    citations[track["track_uri"]] = 1
 
-    years = collections.OrderedDict(sorted(years.items()))
-    l = list(years.keys())
-    print("First year {}, last year {}".format(l[0], l[-1]))
-    cnt = 0
+    if dataset != "mpd":
+        # only papers from 1970 
+        years = from_to_key(years, 1970)
+        years = collections.OrderedDict(sorted(years.items()))
+        l = list(years.keys())
+        print("First year {}, last year {}".format(l[0], l[-1]))
+        cnt = 0
 
-    for key, value in years.items():
-        cnt += value
-        if cnt/len(papers) >= 0.9:
-            print("90:10 ratio at year {}".format(key))
-            break
+        for key, value in years.items():
+            cnt += value
+            if cnt/len(papers) >= 0.9:
+                print("90:10 ratio at year {}".format(key))
+                break
 
-    print("Plotting paper distribution by year on file")
-    # plot papers from 1970
-    plot(years, dataset, "year", 1970)
-    if dataset == "acm" or dataset == "swp":
+        print("Plotting paper distribution by year on file")
+        plot(years, dataset, "year")
+
+    if dataset == "acm" or dataset == "swp" or dataset == "mpd":
+        if dataset == "acm":
+            text = "citations"
+        elif dataset == "swp":
+            text = "labels"
+        else:
+            text = "tracks"
+        print("Generating {} distribution".format(text))
         citations = paper_by_n_citations(citations)
-
+    # only papers with at least 100 citations
+    # citations = from_to_key(citations, 100)
+    # only papers with max 200 citations
+    citations = from_to_key(citations, 10, 800)
     citations = collections.OrderedDict(sorted(citations.items()))
-    x_dim = "citations" if dataset != "swp" else "occurrences"
-    print("Plotting paper distribution by number of {} on file".format(x_dim))
-    # plot papers with at least 100 citations
-    plot(citations, dataset, "number of {}".format(x_dim), 100)
-
+    x_dim = "Citations" if dataset != "swp" and dataset != "mpd" else "Occurrences"
+    print("Plotting paper distribution by number of {} on file".format(x_dim.lower()))
+    # show the y-value for the bar at x=200 in the plot
+    plot(citations, dataset, x_dim, 200)
+    # show no y-value for any bar
+    # plot(citations, dataset, x_dim)
     print("Unpacking {} data...".format(dataset))
-    if dataset != "swp":
+    if dataset == "acm" or dataset == "dblp":
         bags_of_papers, ids, side_info = unpack_papers(papers)
+    elif dataset == "mpd":
+        # not bags_of_papers but bugs_of_tracks
+        bags_of_papers, ids, side_info = unpack_playlists(papers)
     else:
         bags_of_papers, ids, side_info = unpack_papers_fiv(papers)
     bags = Bags(bags_of_papers, ids, side_info)
 
 else:
+    print("Loading {}".format(path))
+    df = pd.read_csv(path, sep="\t", dtype=str, error_bad_lines=False)
+    # replace nan with empty string
+    df = df.replace(np.nan, "", regex=True)
+    citations = {}
+    for index, paper in df.iterrows():
+        for ref in paper["set"].split(","):
+            if ref == "":
+                continue
+            try:
+                citations[ref] += 1
+            except KeyError:
+                citations[ref] = 1
+
+    print(citations)
+    citations = paper_by_n_citations(citations)
+    print(citations)
+    # only papers with at least 10 citations
+    citations = from_to_key(citations, 0)
+    # only papers with min 10 and max 100 citations
+    # citations = from_to_key(citations, 1, 500)
+    citations = collections.OrderedDict(sorted(citations.items()))
+    x_dim = "Citations" if dataset == "pubmed" else "Occurrences"
+    print("Plotting paper distribution by number of {} on file".format(x_dim.lower()))
+    # show the y-value for the bar at x=50 in the plot
+    # plot(citations, dataset, x_dim, 100)
+    # show no y-value for any bar
+    plot(citations, dataset, x_dim)
+
     print("Unpacking {} data...".format(dataset))
     bags = Bags.load_tabcomma_format(path, unique=True)
 
