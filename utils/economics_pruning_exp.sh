7b91219e6baa973b391fd69e0c81ef2a80d83038
ivagliano <i.vagliano@zbw.eu>
2019-08-10 13:33:15 +0200

    Run Econis with more metadata

--- utils/economics_pruning_exp.sh
+++ utils/economics_pruning_exp.sh
@@ -1,11 +1,15 @@
-DATASET_PATH=/data21/lgalke/datasets/econbiz62k.tsv
+#DATASET_PATH=/data21/lgalke/datasets/econbiz62k.tsv
 DATASET_YEAR=2012
-OUTPUT_PREFIX=/data22/ivagliano/cit2vec-journal-results/econis-vae-dae/titles-only
-RUN=3
+OUTPUT_PREFIX=/data22/ivagliano/cit2vec-journal-results/econis/generic-condition
+RUN=1
 mkdir -p $OUTPUT_PREFIX
-for THRESHOLD in 1 2 3 4 5 10 15 20 25
+for THRESHOLD in 1 2 3 4 5 10 15
 do
-  echo python3 ../main.py $DATASET_PATH $DATASET_YEAR -m $THRESHOLD -o $OUTPUT_PREFIX/econis-$DATASET_YEAR-$THRESHOLD-$RUN.txt
-  python3 ../main.py $DATASET_PATH $DATASET_YEAR -m $THRESHOLD -o $OUTPUT_PREFIX/econis-$DATASET_YEAR-$THRESHOLD-$RUN.txt
+  # With TSV file
+  # echo python3 ../main.py $DATASET_PATH $DATASET_YEAR -m $THRESHOLD -o $OUTPUT_PREFIX/econis-$DATASET_YEAR-$THRESHOLD-$RUN.txt
+  # python3 ../main.py $DATASET_PATH $DATASET_YEAR -m $THRESHOLD -o $OUTPUT_PREFIX/econis-$DATASET_YEAR-$THRESHOLD-$RUN.txt
+  # With JSON file
+  echo python3 ../eval/econis.py $DATASET_YEAR -m $THRESHOLD -o $OUTPUT_PREFIX/econis-$DATASET_YEAR-$THRESHOLD-$RUN.txt
+  python3 ../eval/econis.py $DATASET_YEAR -m $THRESHOLD -o $OUTPUT_PREFIX/econis-$DATASET_YEAR-$THRESHOLD-$RUN.txt
 done
 exit 0
